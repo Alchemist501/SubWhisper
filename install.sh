765e5fea28e6124ad2484a9c589f28b923c7c890
Alchemist501 <siya123jc@gmail.com>
2025-07-26 18:20:15 +0530

    Initial phase done

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "[*] Installing Subwhisper..."
+
+# Check for root or sudo privileges
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root or use sudo"
+    exit 1
+fi
+
+# Make sure main script is executable
+chmod +x subwhisper
+
+# Create directories
+install_dir="/usr/local/share/subwhisper"
+mkdir -p "$install_dir/templates"
+
+# Copy files (not move)
+cp subwhisper /usr/local/bin/subwhisper
+cp templates/index.html.tpl "$install_dir/templates/index.html.tpl"
+cp templates/report.html.tpl "$install_dir/templates/report.html.tpl"
+
+echo "[+] Installation complete!"
+echo "You can now run the tool using: subwhisper"
